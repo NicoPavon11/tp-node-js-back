0e8273903bfcf4d3c2d77a0a7db7463f2d0af90c
camilopezzoni <144172852+camilopezzoni@users.noreply.github.com>
2024-07-04 13:44:33 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'mysql-grupo11.alwaysdata.net',
     user: 'grupo11',
-    password: 'Nsnada_22',
+    password: 'grupo_11',
     database: 'grupo11_db_autos'
 });
 
